8515336e47b486769ea4071ea52a3c531148d341
xurong <3220728192@qq.com>
2021-11-11 08:38:27 +0800

    mybatis入门实例

---
+++ day01_mybatis1_beginner/src/main/java/com/xu/dao/IUserDao.java
@@ -0,0 +1,15 @@
+package com.xu.dao;
+
+import com.xu.domain.User;
+
+import java.util.List;
+
+/**
+ * @auther xurong
+ * @date 2021/10/16 - 10:07
+ * 用户的持久层接口
+ */
+public interface IUserDao {
+    //定义一个方法，返回值都是User对象的List集合
+    List<User> findAll();
+}
